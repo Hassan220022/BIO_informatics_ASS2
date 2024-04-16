4273c3f5c40e6162a206547d036f226a5d8b6a72
Hassan220022 <hassansonson2002@gmail.com>
2024-04-16 20:47:10 +0200

    changes

--- aaa.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# Load the content of the Input.txt file to examine the sequences and prepare them for alignment
-file_path_input_txt = 'Input.txt'
-
-# Open and read the content of the file to display and check the format
-with open(file_path_input_txt, 'r') as file:
-    input_txt_content = file.read()
-
-input_txt_content[:1000]  # Displaying the first 1000 characters to check the format without overloading the output
